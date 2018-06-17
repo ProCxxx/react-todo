feed5ebb83b694163ee308561c3b46438d79f94a
root <root@localhost.localdomain>
2018-06-17 04:10:46 +0200

    First

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
+
+import Logo from "./components/Logo";
+import Menu from "./components/Menu";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeComponent: <Logo />
+    };
+  }
+  componentDidMount = () => {
+    setTimeout(2000, () => {
+      this.setState({ activeComponent: <Menu /> });
+    });
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        {this.state.activeComponent}
+        <Text> LOL </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// AppRegistry.registerComponent("App", () => <App />);
---
+++ components/Logo.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Quiz by Pr0xY & ...</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    color: "#000",
+    fontWeight: "800",
+    fontSize: 24,
+    textAlign: "center"
+  }
+});
